67600362823683dd1a58b94f52adbecbc595fba1
“elfacu0” <mahat.mejor@gmail.com>
2023-01-24 12:55:38 -0600

    Add repository module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { RepositoryModule } from './repository/repository.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, RepositoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
