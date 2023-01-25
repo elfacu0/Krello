3b08159fe9aefffea5e2e828d6c8efb3c3da51c7
“elfacu0” <mahat.mejor@gmail.com>
2023-01-25 07:30:05 -0600

    Add register and login routes to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @HttpCode(HttpStatus.CREATED)
+    @Post('register')
+    register(@Body() dto: RegisterDto) {
+        return this.authService.register(dto);        
+    }
+
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    login(@Request() req) {
+        console.log(req.user);
+        return req.user;
+    }
+}
