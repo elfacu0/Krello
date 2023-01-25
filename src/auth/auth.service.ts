f04503619440ff86693fa62337d87ba87dcc8baa
“elfacu0” <mahat.mejor@gmail.com>
2023-01-25 07:30:42 -0600

    Add register and validate user login in service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as bcrypt from 'bcrypt';
+import { RepositoryService } from 'src/repository/repository.service';
+import { UsersService } from 'src/users/users.service';
+import { LoginDto, RegisterDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly usersService: UsersService,
+        private readonly repository: RepositoryService) { }
+
+    async validateUser(username: string, password: string) {
+        const user = await this.usersService.getUser(username);
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        if (user && user.hashedPassword === hashedPassword) {
+            const { hashedPassword, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    async register(dto: RegisterDto) {
+        const { username, email, password, confirmPassword } = dto;
+        if (password != confirmPassword) throw new ForbiddenException("Passwords doesnt match");
+        const hashedPassword = await bcrypt.hash(password, 10);
+        try {
+            const user = await this.repository.user.create({
+                data: {
+                    username,
+                    email,
+                    hashedPassword
+                }
+            })
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError && error.code == 'P2002') {
+                throw new ForbiddenException("Credentials Taken");
+            }
+            throw error;
+        }
+    }
+
+    async login(dto: LoginDto) { }
+}
