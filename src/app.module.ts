ba6ed02c7ea25471f51783363aa870b6a325bcd5
“elfacu0” <mahat.mejor@gmail.com>
2023-01-25 07:31:46 -0600

    Import neccesary files to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { RepositoryModule } from './repository/repository.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [UsersModule, RepositoryModule],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), AuthModule, UsersModule, RepositoryModule],
   providers: [AppService],
+  controllers: [AppController]
 })
-export class AppModule {}
+export class AppModule { }
