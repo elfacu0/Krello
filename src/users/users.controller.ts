9e7ad57e7bc5afa269d0a2197886562f3d7172ba
“elfacu0” <mahat.mejor@gmail.com>
2023-01-25 15:51:06 -0600

    Add user module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Patch, UseGuards, Request, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { EditUserDto } from './dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+
+    // @HttpCode(HttpStatus.OK)
+    // @Get(":username")
+    // getUser(@Param('username') username: string) {
+    //     return this.usersService.getUser(username);
+    // }
+
+    @HttpCode(HttpStatus.OK)
+    @Get(":id")
+    getUserById(@Param('id', ParseIntPipe) userId: number) {
+        return this.usersService.getUserById(userId);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(JwtAuthGuard)
+    @Patch("edit")
+    editProfile(@Request() req, @Body() dto: EditUserDto) {
+        const { user } = req;
+        return this.usersService.editUser(user.id, dto);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { RepositoryService } from 'src/repository/repository.service';
+import { EditUserDto } from './dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly repository: RepositoryService) { }
+
+    async getUser(username: string): Promise<User | undefined> {
+        const user = await this.repository.user.findFirst({
+            where: {
+                username: username
+            }
+        });
+        return user;
+    }
+
+    async getUserById(userId: number) {
+        if (userId === undefined) throw new BadRequestException('parameter error', { cause: new Error(), description: 'userId needs to be a number' })
+        const { hashedPassword, ...user } = await this.repository.user.findUnique({
+            where: {
+                id: userId
+            }
+        });
+        return user;
+    }
+
+    async editUser(id: number, dto: EditUserDto) {
+        const user: User = await this.repository.user.update({
+            where: {
+                id
+            },
+            data: {
+                ...dto
+            }
+        })
+
+    }
+}
